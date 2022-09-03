61ca89d2794146ca3aa45949b6c3086ef6415e85
AnishDaCoder <96700110+AnishDaCoder@users.noreply.github.com>
2022-09-02 18:54:35 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function startClassification(){
-    navigator.mediaDevices.getUserMedia({audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/BVh_flLHx/', modelReady)
-}
-
-function modelReady(){
-    classifier.classify(gotResults);
-}
